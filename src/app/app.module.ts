1aeba53c3b11afad3f95028c20d4ee1b2b816718
averyferrante <averyferrante123@gmail.com>
2018-12-21 16:19:14 -0600

    Added a quick nav. Set up initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: MainComponent }
+    ])
   ],
   providers: [],
   bootstrap: [MainComponent]
