f4ad696101640f38c564c39d6b35483d9f01ddb2
averyferrante <averyferrante123@gmail.com>
2019-02-25 19:47:36 -0600

    Getting started again

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment';
-import { LoginCredentials } from 'src/models/login credentials';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  baseRoute = environment.APIEndpoint + 'users/';
-  constructor(private http: HttpClient) { }
-
-  validateUser(credentials: LoginCredentials): Observable<number> {
-    return this.http.post<number>(this.baseRoute + 'validate', credentials);
-  }
+  baseRoute = environment.APIEndpoint + 'users';
+  constructor() { }
 }
