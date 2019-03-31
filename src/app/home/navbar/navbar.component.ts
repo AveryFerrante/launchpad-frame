a56e4b121cd26cc3d478a682b34afd5c0701a6c5
averyferrante <averyferrante123@gmail.com>
2019-03-31 18:09:11 -0500

    small CSS changes pulled in a css toggle button

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -8,13 +8,22 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
+  frames = false;
+  pictures = false;
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  click() {
-    this.router.navigate(['home', 'frames']);
+  click(destination: string) {
+    this.router.navigate(['home', destination]);
+    if (destination === 'frames') {
+      this.frames = true;
+      this.pictures = false;
+    } else {
+      this.frames = false;
+      this.pictures = true;
+    }
   }
 
 }
