686942d18760c3990003b1e48e02caa7880bd9ef
averyferrante <AFerrante@ethosgroup.com>
2019-04-24 11:04:48 -0500

    Figuring out sidenav sizing... Using plain JS

--- src/app/frames/frames/frames.component.ts
+++ src/app/frames/frames/frames.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, AfterViewInit, AfterViewChecked, HostListener } from '@angular/core';
 import { FramesService } from 'src/app/services/frames/frames.service';
 import { Frame } from 'src/app/models/Frame';
-import { concatMap, take } from 'rxjs/operators';
+import { concatMap, take, filter, debounceTime } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { GlobalEventsService } from 'src/app/services/global/global-events.service';
 import { trigger, style, animate, transition } from '@angular/animations';
@@ -9,26 +9,14 @@ import { trigger, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-frames',
   templateUrl: './frames.component.html',
-  styleUrls: ['./frames.component.css'],
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(-100%)', opacity: .75}),
-          animate('150ms', style({transform: 'translateX(0)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1}),
-          animate('0ms', style({transform: 'translateX(-100%)', opacity: .75}))
-        ])
-      ]
-    )
-  ],
+  styleUrls: ['./frames.component.css']
 })
 export class FramesComponent implements OnInit, OnDestroy {
 
   showSidenav = true;
   overlaySidebar = false;
+  hasBeenHiddenOnSmall = false; // Used so we only auto hide sidenav once after going small
+  hasBeenShownOnBig = false; // Used so we only auto show sidenav once after getting big
   constructor(private globalEventsService: GlobalEventsService) { }
   // Handle sidenav show/hide/toggle
   @HostListener('window:resize', ['$event']) onResize(event) {
@@ -38,18 +26,34 @@ export class FramesComponent implements OnInit, OnDestroy {
     // Create Macrotask to avoid updating out of check. May need to redesign....
     setTimeout(() => this.globalEventsService.showHamburger(true));
     this.globalEventsService.showSidenavEmitter.subscribe(
-      (show) => this.showSidenav = show
+      (show) => {
+        this.showSidenav = show;
+        if (window.innerWidth < 992 && this.showSidenav === true && this.hasBeenHiddenOnSmall) {
+          setTimeout(() => { const navbar = document.getElementById('topNav');
+          const sidenav = document.getElementById('frameSidenav');
+          console.log('fired sidenav sizing logic');
+          sidenav.setAttribute('style', sidenav.getAttribute('style') + ' ' +
+            `height: calc(${document.body.scrollHeight}px - ${navbar.offsetHeight}px);`); }, 1);
+        }
+      }
     );
-    this.checkWidth(window.innerWidth);
   }
 
   private checkWidth(width) {
     if (width < 992) {
-      this.globalEventsService.showSidenav(false);
-      this.overlaySidebar = true;
+      if (!this.hasBeenHiddenOnSmall) {
+        this.globalEventsService.showSidenav(false);
+        this.hasBeenHiddenOnSmall = true;
+        this.hasBeenShownOnBig = false;
+        this.overlaySidebar = true;
+      }
     } else {
-      this.globalEventsService.showSidenav(true);
-      this.overlaySidebar = false;
+      if (!this.hasBeenShownOnBig) {
+        this.globalEventsService.showSidenav(true);
+        this.overlaySidebar = false;
+        this.hasBeenHiddenOnSmall = false;
+        this.hasBeenShownOnBig = true;
+      }
     }
   }
 
