7bbb967ae908ddff4a14a85d2a75be88e3e8d6fc
averyferrante <averyferrante123@gmail.com>
2019-06-04 07:58:19 -0500

    Notification Sidenav opens/closes as expected Project content for reuse? Mobile view is a bit off

--- src/app/notifications/sidenav/sidenav.component.ts
+++ src/app/notifications/sidenav/sidenav.component.ts
@@ -1,25 +1,56 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { tap, skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-notification-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
 
   _show = false;
-  @Input() set showNotificationSidenav(val: boolean) { this._show = val; }
-  // tslint:disable-next-line: no-output-rename
-  @Output('showNotificationSidenav') showOutputEvent = new EventEmitter();
+  @Input() set showNotificationSidenav(val: boolean) { this._show = val; this.handleClickListener(); }
   get showNotificationSidenav() { return this._show; }
+  @Output() showNotificationSidenavChange = new EventEmitter<boolean>();
+  clickSub: Subscription = null;
   constructor() { }
 
-  ngOnInit() {
-    const a = fromEvent(document, 'click').pipe(
-      tap((e) => console.log(e))
+  ngOnInit() { }
+
+  onClose() {
+    this.showNotificationSidenav = false;
+    this.showNotificationSidenavChange.emit(false);
+  }
+
+  handleClickListener() {
+    if (this.showNotificationSidenav) {
+      this.initClickListener();
+    } else {
+      this.destroyClickListener();
+    }
+  }
+
+  initClickListener() {
+    this.clickSub = fromEvent(document, 'click').pipe(
+      skip(1), // Event fired for clicking notification icon to open
+      tap((e: MouseEvent) => {
+        const element = e.target as Element;
+        if (element.className === 'opaque-bg') {
+          this.onClose();
+        }
+      })
     ).subscribe();
   }
 
+  destroyClickListener() {
+    if (this.clickSub !== null) {
+      this.clickSub.unsubscribe();
+      this.clickSub = null;
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroyClickListener();
+  }
 }
