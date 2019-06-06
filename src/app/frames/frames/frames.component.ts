f98ceccc95c4f378de4f62d37f05d6ba9b223db7
averyferrante <averyferrante123@gmail.com>
2019-06-06 07:52:59 -0500

    Fixed routing behavior for 'frames' path Small notification UI change Added better break points for frame image viewing Thinking about sidenav for frames screen...

--- src/app/frames/frames/frames.component.ts
+++ src/app/frames/frames/frames.component.ts
@@ -1,80 +1,39 @@
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 import { UserInfo } from 'src/app/models/UserInfo';
-import { GlobalEventsService } from 'src/app/services/global/global-events.service';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-frames',
   templateUrl: './frames.component.html',
   styleUrls: ['./frames.component.css']
 })
-export class FramesComponent implements OnInit, OnDestroy {
+export class FramesComponent implements OnInit {
 
-  showSidenav = true;
-  overlaySidebar = false;
-  hasBeenHiddenOnSmall = false; // Used so we only auto hide sidenav once after going small
-  hasBeenShownOnBig = false; // Used so we only auto show sidenav once after getting big
   userInfo$: Observable<UserInfo>;
   sidenavSubscription: Subscription;
-  constructor(private globalEventsService: GlobalEventsService, private route: ActivatedRoute, private router: Router) { }
-  // Handle sidenav show/hide/toggle
-  @HostListener('window:resize', ['$event']) onResize(event) {
-    this.checkWidth();
-  }
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
   ngOnInit() {
-    // Create Macrotask to avoid updating out of check. May need to redesign....
-    setTimeout(() => this.globalEventsService.showHamburger(true));
     this.userInfo$ = (this.route.snapshot.data['UserInfo'] as Observable<UserInfo>);
     this.userInfo$.pipe(
-      map((ui: UserInfo) => {
-        if (ui.frames && Object.keys(ui.frames).length > 0) {
-          const frameId = Object.keys(ui.frames)[0];
-          this.router.navigate(['/frames', frameId]);
-        } else {
-          this.router.navigate(['/frames', 'create']);
+      take(1),
+      tap((ui: UserInfo) => {
+        const urlSegments = this.router.url.split('/');
+        const deepestPath = urlSegments[urlSegments.length - 1];
+        if (deepestPath.toLowerCase() === 'create' || (ui.frames && deepestPath in ui.frames)) {
+          return;
+        } else { // Route them to create frame or an existing frame (can't just go to '/frames')
+          if (ui.frames && Object.keys(ui.frames).length > 0) {
+            const frameId = Object.keys(ui.frames)[0];
+            this.router.navigate(['/frames', frameId]);
+          } else {
+            this.router.navigate(['/frames', 'create']);
+          }
         }
       })
-    ).subscribe().unsubscribe();
-    this.sidenavSubscription = this.globalEventsService.showSidenavEmitter.subscribe(
-      (show) => {
-        this.showSidenav = show;
-        if (this.showSidenav === true && this.hasBeenHiddenOnSmall) {
-          setTimeout(() => {
-            const navbar = document.getElementById('topNav');
-            const sidenav = document.getElementById('frameSidenav');
-            sidenav.setAttribute('style', sidenav.getAttribute('style') + ' ' +
-              `height: calc(${document.body.offsetHeight}px - ${navbar.offsetHeight}px);`);
-          });
-        }
-      }
-    );
-    this.checkWidth();
+    ).subscribe();
   }
-
-  private checkWidth() {
-    if (window.innerWidth < 992) {
-      if (!this.hasBeenHiddenOnSmall) {
-        this.globalEventsService.showSidenav(false);
-        this.hasBeenHiddenOnSmall = true;
-        this.hasBeenShownOnBig = false;
-        this.overlaySidebar = true;
-      }
-    } else {
-      if (!this.hasBeenShownOnBig) {
-        this.globalEventsService.showSidenav(true);
-        this.overlaySidebar = false;
-        this.hasBeenHiddenOnSmall = false;
-        this.hasBeenShownOnBig = true;
-      }
-    }
-  }
-
-  ngOnDestroy() {
-    this.sidenavSubscription.unsubscribe();
-    this.globalEventsService.showHamburger(false);
-  }
-
 
 }
